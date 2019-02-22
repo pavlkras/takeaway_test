27d27218c496fb776637c74248f52b6b7e09af28
pavel <pavel@weissbeerger.com>
2019-02-22 11:44:20 +0200

    Creating app skeleton: docker compose

--- dockerComposeRun.bat
+++ dockerComposeRun.bat
@@ -1,6 +1,6 @@
-#!/usr/bin/env bash
-gradle assemble
+call gradle assemble
 cd compose
+docker-compose rm -f && docker-compose pull
 docker-compose --file docker-compose.yml build
 docker-compose --file docker-compose.yml up
-# -d to add to detach
+rem -d to add to detach
---
+++ postgres/init_db.sql
@@ -0,0 +1,15 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS departments (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS employees (
+  _uuid uuid PRIMARY KEY DEFAULT uuid_generate_v1(),
+  email VARCHAR(254) NOT NULL UNIQUE,
+  first_name VARCHAR(100) NOT NULL,
+  last_name VARCHAR(100) NOT NULL,
+  department_id INTEGER NOT NULL REFERENCES departments(id) ON DELETE RESTRICT
+);
+
