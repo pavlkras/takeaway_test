845c08d0431a6c03e1d17db87305e60f400d51ed
pavel <pavel@weissbeerger.com>
2019-02-22 17:14:43 +0200

    Employee service: Adding web security

---
+++ app-employee-service/src/main/java/com/takeaway/test/employee/config/WebSecurityConfig.java
@@ -0,0 +1,53 @@
+package com.takeaway.test.employee.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * takeaway_test
+ * <p>
+ * ???
+ *
+ * @author Pavel
+ * @since 22/02/2019
+ */
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST,"/department/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/department/**").hasRole("ADMIN")
+                .and()
+                .httpBasic()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/employee/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/employee/**").hasRole("ADMIN")
+                .and()
+                .httpBasic()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth
+                .inMemoryAuthentication()
+                .withUser("admin").password(encoder.encode("admin")).roles("ADMIN");
+    }
+}
