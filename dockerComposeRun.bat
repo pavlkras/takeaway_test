49966bc2af2937a7ba24fe0d89448282aa57178c
pavel <pavel@weissbeerger.com>
2019-02-23 09:18:18 +0200

    Build: Changing build script

--- dockerComposeRun.bat
+++ dockerComposeRun.bat
@@ -1,6 +1,7 @@
 call gradle assemble
 cd compose
-docker-compose rm -f && docker-compose pull
-docker-compose --file docker-compose.yml build
-docker-compose --file docker-compose.yml up
+call docker-compose rm -f && docker-compose pull
+call docker-compose --file docker-compose.yml build
+call docker-compose --file docker-compose.yml up
 rem -d to add to detach
+cd ..
--- dockerComposeRun.sh
+++ dockerComposeRun.sh
@@ -5,3 +5,4 @@ docker-compose rm -f && docker-compose pull
 docker-compose --file docker-compose.yml build
 docker-compose --file docker-compose.yml up
 # -d to add to detach
+cd ..
